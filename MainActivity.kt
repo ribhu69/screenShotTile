0fee541cb9a52e350a5fe7ccf7415917b8f7a377
Arkaprava Ghosh <62136090+ribhu69@users.noreply.github.com>
2021-03-10 23:30:34 +0530

    Add files via upload
    Initial Commit

---
+++ MainActivity.kt
@@ -0,0 +1,20 @@
+
+package com.example.myapplication
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
---
+++ ScreenShotTile.kt
@@ -0,0 +1,92 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Environment
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import android.util.Log
+import android.view.View
+import androidx.annotation.RequiresApi
+import java.io.File
+import java.io.FileOutputStream
+
+
+@Suppress("DEPRECATION")
+@RequiresApi(Build.VERSION_CODES.N)
+class ScreenShotTile : TileService() {
+
+    private lateinit var rootView : View
+
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+        Log.d(TAG, "onTileAdded")
+    }
+
+
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+        Log.d(TAG, "onTileAdded")
+    }
+
+
+    override fun onClick() {
+
+
+        Log.d(TAG, "onClick" + qsTile.state.toString())
+        takeScreenshot()
+
+    }
+
+    override fun onStartListening() {
+
+        val tile: Tile = qsTile
+        Log.d(TAG, "onStartListening" + tile.label)
+    }
+
+    override fun onStopListening() {
+        super.onStopListening()
+        Log.d(TAG, "onStopListening")
+    }
+
+
+
+    private fun takeScreenshot() {
+        val closeIntent = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
+        sendBroadcast(closeIntent)
+
+        rootView = Activity().window.decorView.rootView as View
+
+
+        // create bitmap screen capture
+        try {
+
+
+            rootView.isDrawingCacheEnabled = true
+            val bitmap = Bitmap.createBitmap(rootView.drawingCache)
+            rootView.isDrawingCacheEnabled = false
+
+            //val mPath: String = Environment.getExternalStorageDirectory().toString() + "/" + "now" + ".jpg"
+
+            val mPath: String = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString() + "/" + "arka" + ".jpg"
+            val imageFile = File(mPath)
+
+
+            //Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val outputStream = FileOutputStream(imageFile)
+            val quality = 100
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
+            outputStream.flush()
+            outputStream.close()
+            Log.d(TAG,"Operation Completed")
+            //Toast.makeText(this, "Captured", Toast.LENGTH_SHORT).show()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+
+    }
+}
